b2992002e7c3dc24587db7c9735ffa4d315e653b
dboikliev <deyanpb@hotmail.com>
2017-11-20 19:39:03 +0200

    Testing experimental caching aspect.

--- build/src/app.js
+++ build/src/app.js
@@ -7,21 +7,65 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const aspect_1 = require("./aspect");
-class TestAspect extends aspect_1.SurroundAspect {
+class MemoryCache {
+    constructor() {
+        this.elements = new Map();
+    }
+    get(id) {
+        return this.elements.get(id);
+    }
+    has(id) {
+        return this.elements.has(id);
+    }
+    set(id, element, period) {
+        this.elements.set(id, element);
+        if (typeof period !== "undefined") {
+            setTimeout((cache) => {
+                cache.invalidate(id);
+            }, period, this);
+        }
+    }
+    invalidate(id) {
+        if (this.elements.has(id)) {
+            this.elements.delete(id);
+        }
+    }
+}
+class Cached extends aspect_1.SurroundAspect {
+    constructor(cachingService) {
+        super();
+        this.cachingService = cachingService;
+    }
     onInvoke(func) {
-        return function (...args) {
-            let result = func.apply(this, args);
+        const cache = this.cachingService;
+        return function (id) {
+            if (cache.has(id)) {
+                return cache.get(id);
+            }
+            const result = func.call(this, id);
+            cache.set(id, result);
             return result;
         };
     }
 }
-let TestSubject = class TestSubject {
-    testMethod() {
+function createCachingAspect(cachingService) {
+    return aspect_1.aspect.bind(null, new Cached(cachingService), aspect_1.Target.All ^ aspect_1.Target.Constructor);
+}
+const cachingAspect = createCachingAspect(new MemoryCache());
+let UserService = class UserService {
+    getUserById(id) {
+        console.log("In get user by id");
+        return {
+            name: "Ivan",
+            age: 21
+        };
     }
 };
-TestSubject = __decorate([
-    aspect_1.aspect(new TestAspect())
-], TestSubject);
-let test = new TestSubject();
-test.testMethod();
+UserService = __decorate([
+    cachingAspect()
+], UserService);
+const us = new UserService;
+const first = us.getUserById(1);
+const second = us.getUserById(1);
+console.log(first == second);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -9,20 +9,81 @@ import {
     error
 } from "./aspect";
 
-class TestAspect extends SurroundAspect {
+interface CachingService<T> {
+    get(id: string | number): T
+    set(id: string | number, element: T, period?: number): void
+    has(id: string | number): boolean
+    invalidate(id: string | number): void
+}
+
+class MemoryCache<T> implements CachingService<T> {
+    private elements: Map<string | number, T> = new Map();
+
+    get(id: string | number): T {
+        return this.elements.get(id);
+    }
+
+    has(id: string | number): boolean {
+        return this.elements.has(id);
+    }
+
+    set(id: string | number, element: T, period?: number): void {
+        this.elements.set(id, element);
+        if (typeof period !== "undefined") {
+            setTimeout((cache: MemoryCache<T>) => {
+                cache.invalidate(id);
+            }, period, this)
+        }
+    }
+
+    invalidate(id: string | number): void {
+        if (this.elements.has(id)) {
+            this.elements.delete(id);
+        }
+    }
+}
+
+class Cached<T> extends SurroundAspect {
+    constructor(private cachingService: CachingService<T>) {
+        super();
+    }
+
     onInvoke(func: Function): Function {
-        return function (...args) {
-            let result = func.apply(this, args);
+        const cache = this.cachingService;
+        return function (id: string | number) {
+            if (cache.has(id)) {
+                return cache.get(id);
+            }
+            const result = func.call(this, id);
+            cache.set(id, result);
             return result;
         }
     }
 }
 
-@aspect(new TestAspect())
-class TestSubject {
-    testMethod() {
-    }
-}  
+function createCachingAspect<T>(cachingService: CachingService<T>) {
+    return aspect.bind(null, new Cached(cachingService), Target.All ^ Target.Constructor);
+}
 
-let test = new TestSubject();
-test.testMethod();
+const cachingAspect = createCachingAspect(new MemoryCache<User>())
+
+@cachingAspect()
+class UserService {
+    getUserById(id: number): User {
+        console.log("In get user by id");
+        return {
+            name: "Ivan",
+            age: 21
+        }
+    }
+}
+
+interface User {
+    name: string,
+    age: number
+}
+
+const us = new UserService
+const first = us.getUserById(1)
+const second = us.getUserById(1);
+console.log(first == second);
